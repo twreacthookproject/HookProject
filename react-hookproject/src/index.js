1e2546edd5d8acd2ad92010daa17f23edcc5f8cf
Huseyin Toprak <HuseyinToprak96@outlook.com>
2023-06-13 15:17:34 +0300

    update

--- react-hookproject/src/index.js
+++ react-hookproject/src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import {Provider} from "react-redux"
+import configureStore from "./redux/reducers/configureStore"
+import {BrowserRouter} from "react-router-dom"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const store = configureStore();
+ReactDOM.render(<BrowserRouter><Provider store={store}><App/></Provider></BrowserRouter>, document.getElementById("root"));
 registerServiceWorker();
