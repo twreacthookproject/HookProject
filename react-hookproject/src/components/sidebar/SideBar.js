e684cc3db1bf39c4785011b6e0034fc95b264e6e
Resul Pala <resulpala@hotmail.com.tr>
2023-06-14 09:23:12 +0300

    calisanHaliAmaAz

--- react-hookproject/src/components/sidebar/SideBar.js
+++ react-hookproject/src/components/sidebar/SideBar.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { connect } from "react-redux";
+import { AllCategories } from "../../redux/actions/categoryActions";
 
 import {
   faHome,
@@ -15,55 +16,66 @@ import classNames from "classnames";
 import { Link } from "react-router-dom";
 import SubMenu from "./SubMenu";
 
-const SideBar = ({ isOpen, toggle }) => (
-  <div className={classNames("sidebar", { "is-open": isOpen })}>
-    <div className="sidebar-header">
-      <span color="info" onClick={toggle} style={{ color: "#fff" }}>
-        &times;
-      </span>
-      <h3>NOTES</h3>
+function SideBar({ isOpen, toggle, categoriesProp, ...props }) {
+  const [categories, setCategories] = useState({ ...categoriesProp });
+
+  useEffect(() => {
+    if (categories.length === 0) {
+      props.dispatch(AllCategories());
+    }
+    setCategories({ ...props.product });
+  }, [props.categories]);
+
+  return (
+    <div className={classNames("sidebar", { "is-open": isOpen })}>
+      <div className="sidebar-header">
+        <span color="info" onClick={toggle} style={{ color: "#fff" }}>
+          &times;
+        </span>
+        <h3>NOTES</h3>
+      </div>
+      <div className="side-menu">
+        <Nav vertical className="list-unstyled pb-3">
+          <p>Menu</p>
+          <NavItem>
+            <NavLink tag={Link} to={"/home"}>
+              <FontAwesomeIcon icon={faBriefcase} className="mr-2" />
+              Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to={"/about"}>
+              <FontAwesomeIcon icon={faBriefcase} className="mr-2" />
+              About
+            </NavLink>
+          </NavItem>
+          <SubMenu
+            title="Categories"
+            icon={faCopy}
+            items={Object.values(categories)}
+          />
+          <NavItem>
+            <NavLink tag={Link} to={"/pages"}>
+              <FontAwesomeIcon icon={faCopy} className="mr-2" />
+              New Note
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to={"/contact"}>
+              <FontAwesomeIcon icon={faPaperPlane} className="mr-2" />
+              Contact
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </div>
     </div>
-    <div className="side-menu">
-      <Nav vertical className="list-unstyled pb-3">
-        <p>Menu</p>
-        <NavItem>
-          <NavLink tag={Link} to={"/home"}>
-            <FontAwesomeIcon icon={faBriefcase} className="mr-2" />
-            Home
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} to={"/about"}>
-            <FontAwesomeIcon icon={faBriefcase} className="mr-2" />
-            About
-          </NavLink>
-        </NavItem>
-        <SubMenu
-          title="Categories"
-          icon={faCopy}
-          items={this.props.categories}
-        />
-        <NavItem>
-          <NavLink tag={Link} to={"/pages"}>
-            <FontAwesomeIcon icon={faCopy} className="mr-2" />
-            New Note
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} to={"/contact"}>
-            <FontAwesomeIcon icon={faPaperPlane} className="mr-2" />
-            Contact
-          </NavLink>
-        </NavItem>
-      </Nav>
-    </div>
-  </div>
-);
+  );
+}
 
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
-    categories: state.categoryListReducer,
+    categoriesProp: state.categoryListReducer,
   };
 }
 
